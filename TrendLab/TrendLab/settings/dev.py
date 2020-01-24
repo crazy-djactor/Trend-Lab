e3b414ef8843d0b119cc2ebd33ee11e4c5c5f7c0
Hector Vidal <hectorvidal@Hectors-MacBook-Pro.local>
2020-01-24 22:44:01 +0000

    Some changes

--- TrendLab/TrendLab/settings/dev.py
+++ TrendLab/TrendLab/settings/dev.py
@@ -17,7 +17,7 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "hectorvidal",
+        "NAME": "trendlab_db",
         "USER": "hectorvidal",
         "PASSWORD": "",
         "HOST": "localhost",
