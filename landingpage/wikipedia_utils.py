e25b18b0abfca756bb5ceb23b1bf91a4beccfa15
Patrick Mutuku <patrick.mutuku@minerva.kgi.edu>
2020-02-11 22:50:46 +0800

    Fix wikipedia error by handling the exception, detailpage showing only search bar when no query present

--- landingpage/wikipedia_utils.py
+++ landingpage/wikipedia_utils.py
@@ -1,7 +1,10 @@
 import wikipedia
 
 def get_wikipedia_summary(term,char_limit=280):
-    summary = wikipedia.summary(term)
+    try:
+        summary = wikipedia.summary(term)
+    except:
+        return ''
 
     try:
         summary = summary[0:char_limit]
